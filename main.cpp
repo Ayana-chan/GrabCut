facac447ccd683f872854f0db11de028613f90be
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2023-05-21 21:32:09 +0800

    演示后

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     cout << "--- MAIN START ---" << endl;
 
     auto grabCutter = make_unique<GrabCutter>(R"(D:\Code\C\clionCpp\GrabCut\output\)");
-    std::string imageFileName = "ex2.jpg";
+    std::string imageFileName = "ex3.jpg";
     grabCutter->start(R"(D:\Code\C\clionCpp\GrabCut\test_textures\)" + imageFileName);
 
     cout << "--- MAIN FINISH ---" << endl;
